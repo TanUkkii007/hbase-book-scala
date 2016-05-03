08f4fae0c077a7df9522f6f7e060d4cfbf236a08
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-05-03 10:42:20 +0900

    add PutListErrorExamples

---
+++ src/main/scala/ch03/client/PutListErrorExample1.scala
@@ -0,0 +1,37 @@
+package ch03.client
+
+import org.apache.hadoop.hbase.client.{Put, ConnectionFactory}
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{TableName, HBaseConfiguration}
+import util.HBaseHelper
+import scala.collection.JavaConverters._
+
+object PutListErrorExample1 extends App {
+
+  val conf = HBaseConfiguration.create()
+
+  val helper = new HBaseHelper(conf)
+
+  val tableName = TableName.valueOf("testtable")
+
+  helper.dropTable(tableName)
+  helper.createTable(tableName, List("colfam1"))
+
+  val connection = ConnectionFactory.createConnection(conf)
+  val table = connection.getTable(tableName)
+
+  val put1 = new Put(Bytes.toBytes("row1"))
+  put1.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"))
+  val put2 = new Put(Bytes.toBytes("row2"))
+  put2.addColumn(Bytes.toBytes("BOGUS"), Bytes.toBytes("qual1"), Bytes.toBytes("val2"))
+  val put3 = new Put(Bytes.toBytes("row2"))
+  put3.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("val3"))
+
+  try {
+    table.put(List(put1, put2, put3).asJava)
+  } finally {
+    table.close()
+    connection.close()
+    helper.close()
+  }
+}
