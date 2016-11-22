394adcc763860c2616a0298fc3adc1a6cc215732
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-11-22 23:56:40 +0900

    [WIP] ImportFromFile

--- src/main/scala/ch07/ImportFromFile.scala
+++ src/main/scala/ch07/ImportFromFile.scala
@@ -1,18 +1,23 @@
 package ch07
 
+import org.apache.commons.cli.{CommandLine, HelpFormatter, PosixParser, Options}
 import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.logging.LogFactory
-import org.apache.hadoop.hbase.KeyValue
+import org.apache.hadoop.hbase.{HBaseConfiguration, KeyValue}
 import org.apache.hadoop.hbase.client.{Put, Mutation}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.io.{Text, LongWritable}
 import org.apache.hadoop.mapreduce.Mapper
+import org.apache.hadoop.util.GenericOptionsParser
+import org.slf4j.Logger
 import scala.collection.JavaConverters._
 
 object ImportFromFile {
 
   private final val log = LogFactory.getLog(this.getClass)
+
+  val NAME = "ImportFromFile"
 
   object Counters extends Enumeration {
     val LINES = Value("LINES")
@@ -38,9 +43,54 @@ object ImportFromFile {
         val put = new Put(rowKey)
         put.addColumn(family, qualifier, Bytes.toBytes(lineString))
         context.write(new ImmutableBytesWritable(rowKey), put)
-
+        context.getCounter("Counters", "LINES").increment(1)
+      } catch {
+        case e: Exception => e.printStackTrace()
       }
-      super.map(key, value, context)
+    }
+  }
+
+  private def parseArgs(args: Array[String]): CommandLine = {
+    val options = new Options()
+    val o1 = new org.apache.commons.cli.Option("t", "table", true, "table to import into (must exist)")
+    o1.setArgName("table-name")
+    o1.setRequired(true)
+    options.addOption(o1)
+    val o2 = new org.apache.commons.cli.Option("c", "column", true, "column to store row data into (must exist)")
+    o2.setArgName("family:qualifier")
+    o2.setRequired(true)
+    options.addOption(o2)
+    val o3 = new org.apache.commons.cli.Option("i", "input", true, "the directory or file to read from")
+    o3.setArgName("path-in-HDFS")
+    o3.setRequired(true)
+    options.addOption(o3)
+    val o4 = new org.apache.commons.cli.Option("d", "debug", false, "switch on DEBUG log level")
+    options.addOption(o4)
+    val parser = new PosixParser()
+    val cmd: CommandLine = try {
+      parser.parse(options, args)
+    } catch {
+      case e: Exception =>
+        System.err.println("ERROR: " + e.getMessage() + "\n")
+        val formatter = new HelpFormatter()
+        formatter.printHelp(NAME + " ", options, true)
+        System.exit(-1)
+        ???
+    }
+
+    if (cmd.hasOption("d")) {
+
+    }
+    cmd
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = HBaseConfiguration.create()
+    val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs
+    val cmd = parseArgs(otherArgs)
+
+    if (cmd.hasOption("d")) {
+      conf.set("conf.debug", "true")
     }
   }
 
